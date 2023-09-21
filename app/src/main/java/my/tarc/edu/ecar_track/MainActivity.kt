7fd375dbe6dfa9ecf51ca49f79f8c92e1968ff22
Yongsheng2 <ysheng916@gmail.com>
2023-09-21 16:13:25 +0800

    Initial

--- app/src/main/java/my/tarc/edu/ecar_track/MainActivity.kt
+++ app/src/main/java/my/tarc/edu/ecar_track/MainActivity.kt
@@ -1,5 +1,7 @@
 package my.tarc.edu.ecar_track
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import my.tarc.edu.ecar_track.databinding.ActivityMainBinding
@@ -10,5 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnClickListener{
+                menuItem ->
+            when (menuItem.id) {
+                R.id.Home -> {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("content://home_page.kt"))
+                    startActivity(intent)
+                }
+                else -> false
+            }
+        }
     }
 }
