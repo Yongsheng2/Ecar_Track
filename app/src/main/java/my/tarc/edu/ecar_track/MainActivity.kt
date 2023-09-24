02be0019f9f2f4a582a1d371fee4f6597868a8e6
Yongsheng2 <ysheng916@gmail.com>
2023-09-24 11:24:27 +0800

    initial

--- app/src/main/java/my/tarc/edu/ecar_track/MainActivity.kt
+++ app/src/main/java/my/tarc/edu/ecar_track/MainActivity.kt
@@ -1,18 +1,28 @@
 package my.tarc.edu.ecar_track
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.PersistableBundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
 import my.tarc.edu.ecar_track.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        }
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = ActivityMainBinding.inflate(inflater, container, false)
+        return binding
+    }
 }
